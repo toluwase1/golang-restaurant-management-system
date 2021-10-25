a98bf19d758a3d78dff96aa80f01b1cce84b932b
toluwase <toluwasethomas1@gmail.com>
2021-10-25 02:19:45 +0100

    main file

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var foodcollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8000"
+	}
+	router := gin.New()
+	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+
+	routes.FoodRoutes(router)
+	routes.InvoiceRoutes(router)
+	routes.MenuRoutes(router)
+	routes.OrderItemRoutes(router)
+	routes.OrderRoutes(router)
+	routes.TableRoutes(router)
+	routes.UserRoutes(router)
+
+	router.Run(":" + port)
+}
--- routes/userRouter.go
+++ routes/userRouter.go
@@ -1,1 +1,1 @@
- 
+package routes
