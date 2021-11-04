3fa66258b12aaeeb1ea3f2e746999d51b9cac48a
toluwase <toluwasethomas1@gmail.com>
2021-11-04 22:25:47 +0100

    Getmenus controller

--- controllers/menuController.go
+++ controllers/menuController.go
@@ -1,9 +1,29 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"log"
+	"net/http"
+	"time"
+)
+//var menuCollection *mongo.Collection = database.OpenCollection(database.Client, "menu")
 
 func GetMenus() gin.HandlerFunc {
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		result,err:=menuCollection.Find(context.TODO(), bson.M{} )
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"ERROR":" error occurred while listing menu items"})
+		}
+		var allMenus [] bson.M
+		if err = result.All(ctx, &allMenus); err != nil{
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, allMenus)
+	}
 }
 
 func GetMenu() gin.HandlerFunc {
